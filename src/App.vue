276d942a5734cbad573c6521f29ab68d127a47c7
Lydia <858284357@qq.com>
2022-10-19 12:30:47 +0800

    第二次初始化

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,16 @@
 <template>
   <div id="app">
     <router-view></router-view>
-    <tab-bar>
-      <tab-bar-item path="/home" activeColor="deeppink">
-        <img slot="item-icon" src="@/assets/img/tabbar/home.svg" alt="" />
-        <img
-          slot="item-icon-active"
-          src="@/assets/img/tabbar/home_active.svg"
-          alt=""
-        />
-        <div slot="item-text">首页</div>
-      </tab-bar-item>
-      <tab-bar-item path="/category" activeColor="deeppink">
-        <img slot="item-icon" src="@/assets/img/tabbar/category.svg" alt="" />
-        <img
-          slot="item-icon-active"
-          src="@/assets/img/tabbar/category_active.svg"
-          alt=""
-        />
-        <div slot="item-text">分类</div>
-      </tab-bar-item>
-      <tab-bar-item path="/cart" activeColor="deeppink">
-        <img slot="item-icon" src="@/assets/img/tabbar/cart.svg" alt="" />
-        <img
-          slot="item-icon-active"
-          src="@/assets/img/tabbar/cart_active.svg"
-          alt=""
-        />
-        <div slot="item-text">购物车</div>
-      </tab-bar-item>
-      <tab-bar-item path="/profile" activeColor="deeppink">
-        <img slot="item-icon" src="@/assets/img/tabbar/profile.svg" alt="" />
-        <img
-          slot="item-icon-active"
-          src="@/assets/img/tabbar/profile_active.svg"
-          alt=""
-        />
-        <div slot="item-text">我的</div>
-      </tab-bar-item>
-    </tab-bar>
+    <main-tab-bar></main-tab-bar>
   </div>
 </template>
 
 <script>
-import TabBar from "@/components/tabbar/TabBar.vue";
-import TabBarItem from "@/components/tabbar/TabBarItem.vue";
+import MainTabBar from "@/components/content/mainTabbar/mainTabBar.vue";
 export default {
   name: "MyApp",
   components: {
-    TabBar,
-    TabBarItem,
+    MainTabBar,
   },
 };
 </script>
